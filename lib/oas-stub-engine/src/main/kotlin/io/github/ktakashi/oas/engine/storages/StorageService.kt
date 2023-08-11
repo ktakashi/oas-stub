e20d66e4b27d3c78b830318b9b06a61d99706ff8
Takashi Kato <ktakashi@ymail.com>
2023-08-11 09:31:33 +0200

    Using parsing service on storage reading

--- lib/oas-stub-engine/src/main/kotlin/io/github/ktakashi/oas/engine/storages/StorageService.kt
+++ lib/oas-stub-engine/src/main/kotlin/io/github/ktakashi/oas/engine/storages/StorageService.kt
@@ -3,24 +3,22 @@ package io.github.ktakashi.oas.engine.storages
 import com.github.benmanes.caffeine.cache.Caffeine
 import com.github.benmanes.caffeine.cache.LoadingCache
 import io.github.ktakashi.oas.engine.apis.ApiPathService
+import io.github.ktakashi.oas.engine.parsers.ParsingService
 import io.github.ktakashi.oas.model.ApiDefinitions
 import io.github.ktakashi.oas.model.PluginDefinition
 import io.github.ktakashi.oas.plugin.apis.Storage
 import io.github.ktakashi.oas.storage.apis.PersistentStorage
 import io.swagger.v3.oas.models.OpenAPI
-import io.swagger.v3.parser.OpenAPIV3Parser
 import jakarta.inject.Inject
 import jakarta.inject.Named
 import jakarta.inject.Singleton
 import java.util.Optional
 
 
-private val openApiV3Parser = OpenAPIV3Parser()
-
-
 @Named @Singleton
 class StorageService
 @Inject constructor(private val apiPathService: ApiPathService,
+                    private val parsingService: ParsingService,
                     private val persistentStorage: PersistentStorage,
                     val sessionStorage: Storage) {
     private val apiDefinitions: LoadingCache<String, Optional<ApiDefinitions>> = Caffeine.newBuilder()
@@ -28,7 +26,7 @@ class StorageService
     private val openApiCache: LoadingCache<String, Optional<OpenAPI>> = Caffeine.newBuilder()
             .build { k -> apiDefinitions[k]
                     .map(ApiDefinitions::api)
-                    .map { s -> openApiV3Parser.readContents(s).openAPI }
+                    .flatMap(parsingService::parse)
             }
 
     fun saveApiDefinitions(name: String, definitions: ApiDefinitions) {
