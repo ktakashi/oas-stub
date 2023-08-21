372b521a4491d9f85a4108cf5b1865c952a24ce2
Takashi Kato <ktakashi@ymail.com>
2023-08-21 23:06:03 +0200

    Adding gradle enterprise plugin to use scan

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,16 @@
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.4.0"
+    id("com.gradle.enterprise") version "3.14.1"
+}
+
+gradleEnterprise {
+    if (System.getenv("CI") != null) {
+        buildScan {
+            publishAlways()
+            termsOfServiceUrl = "https://gradle.com/terms-of-service"
+            termsOfServiceAgree = "yes"
+        }
+    }
 }
 
 rootProject.name = "oas-stub"
