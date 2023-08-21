b0f2198805b54071b9fcc9ec96aaa69b082fd9c0
Takashi Kato <ktakashi@ymail.com>
2023-08-21 22:08:20 +0200

    Using MongoClient provided by Spring data instead of creating own

--- lib/storages/mongodb/oas-stub-mongodb-storage-starter/src/main/kotlin/io/github/ktakashi/oas/storages/mongodb/configurations/AutoMongodbStorageConfigurations.kt
+++ lib/storages/mongodb/oas-stub-mongodb-storage-starter/src/main/kotlin/io/github/ktakashi/oas/storages/mongodb/configurations/AutoMongodbStorageConfigurations.kt
@@ -1,47 +1,26 @@
 package io.github.ktakashi.oas.storages.mongodb.configurations
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.mongodb.ConnectionString
-import com.mongodb.MongoClientSettings
 import com.mongodb.client.MongoClient
-import com.mongodb.client.MongoClients
 import io.github.ktakashi.oas.storage.apis.PersistentStorage
 import io.github.ktakashi.oas.storage.apis.SessionStorage
 import io.github.ktakashi.oas.storages.mongodb.MongodbPersistentStorage
 import io.github.ktakashi.oas.storages.mongodb.MongodbSessionStorage
 import org.springframework.boot.autoconfigure.AutoConfiguration
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
-import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.context.properties.NestedConfigurationProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.util.UriComponentsBuilder
 
-@AutoConfiguration
-@ConditionalOnExpression("'mongodb'.equals('\${oas.storage.type.session}') or 'mongodb'.equals('\${oas.storage.type.persistent}') ")
-@EnableConfigurationProperties(MongodbStorageProperties::class)
-class AutoMongodbConfiguration(private val properties: MongodbStorageProperties) {
-
-    @Bean
-    @ConditionalOnMissingBean(MongoClient::class)
-    fun mongoClient(objectMapper: ObjectMapper): MongoClient {
-        val connection = properties.client ?: throw IllegalStateException("'oas.storage.mongodb.client' is required")
-        val connectionString = connection.toConnectionString()
-        return MongoClients.create(MongoClientSettings.builder()
-                .retryWrites(connection.retryWrites)
-                .retryReads(connection.retryReads)
-                .applyConnectionString(connectionString)
-                .build())
-    }
-}
 
 @AutoConfiguration(
         beforeName = ["io.github.ktakashi.oas.storages.inmemory.configurations.AutoInMemorySessionStorageConfiguration"],
-        after = [AutoMongodbConfiguration::class]
+        after = [MongoAutoConfiguration::class]
 )
 @Configuration
 @ConditionalOnBean(value = [MongoClient::class])
@@ -58,7 +37,7 @@ class AutoMongodbSessionStorageConfiguration(private val properties: MongodbStor
 
 @AutoConfiguration(
         beforeName = ["io.github.ktakashi.oas.storages.inmemory.configurations.AutoInMemoryPersistentStorageConfiguration"],
-        after = [AutoMongodbConfiguration::class]
+        after = [MongoAutoConfiguration::class]
 )
 @Configuration
 @ConditionalOnBean(value = [MongoClient::class])
@@ -76,38 +55,7 @@ class AutoMongodbPersistentStorageConfiguration(private val properties: MongodbS
 
 @ConfigurationProperties(prefix = "oas.storage.mongodb")
 data class MongodbStorageProperties(@NestedConfigurationProperty var session: MongodbConnectionProperties?,
-                                    @NestedConfigurationProperty var persistent: MongodbConnectionProperties?,
-                                    @NestedConfigurationProperty var client: MongoClientProperties?)
+                                    @NestedConfigurationProperty var persistent: MongodbConnectionProperties?)
 
 data class MongodbConnectionProperties(var database: String,
                                        var collection: String)
-
-data class MongoClientProperties(var uri: String? = null,
-                                 var host: String? = null,
-                                 var port: Int? = 27017,
-                                 var username: String? = null,
-                                 var password: String? = null,
-                                 var retryWrites: Boolean = true,
-                                 var retryReads: Boolean = true,
-                                 var ssl: Boolean = false) {
-    fun toConnectionString(): ConnectionString = if (uri.isNullOrBlank()) {
-        ConnectionString(UriComponentsBuilder.newInstance()
-                .scheme("mongodb")
-                .userInfo(toUserInfo())
-                .host(host)
-                .port(port?: 27017)
-                .build().toUriString())
-    } else {
-        ConnectionString(uri!!)
-    }
-
-    private fun toUserInfo(): String? = if (username.isNullOrBlank()) {
-        null
-    } else {
-        if (password.isNullOrBlank()) {
-            username
-        } else {
-            "$username:$password"
-        }
-    }
-}
