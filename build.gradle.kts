ddf276fb0bb9826ad2f7f68ffb8a4fabde12c855
Takashi Kato <ktakashi@ymail.com>
2023-08-23 14:54:16 +0200

    Loading properties file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,17 @@
+import java.util.Properties
+
 description = "OAS stub"
+
+val publishPropertiesFile = file("publish.properties")
+if (publishPropertiesFile.exists()) {
+    publishPropertiesFile.inputStream().buffered().use { s ->
+        Properties().apply {
+            load(s)
+        }
+    }.forEach { k, v ->
+        ext[k as String] = v
+    }
+}
 
 allprojects {
     group = group
