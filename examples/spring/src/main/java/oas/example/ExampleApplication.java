5ff462d62c1fbe283368fdad2ec3f0f03adf8e61
Takashi Kato <ktakashi@ymail.com>
2023-09-05 14:48:31 +0200

    Removing unused imports

--- examples/spring/src/main/java/oas/example/ExampleApplication.java
+++ examples/spring/src/main/java/oas/example/ExampleApplication.java
@@ -1,13 +1,8 @@
 package oas.example;
 
-import io.github.ktakashi.oas.configuration.ExecutorsProperties;
-import io.github.ktakashi.oas.services.DefaultExecutorProvider;
-import io.github.ktakashi.oas.web.services.ExecutorProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @ComponentScan("io.github.ktakashi.oas")
