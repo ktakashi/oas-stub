8669e0c3973139180d1b07d9288666ff52e2d5c4
Takashi Kato <ktakashi@ymail.com>
2023-09-11 11:58:38 +0200

    Make hazelcast credential enhancible

--- app/spring/oas-stub-hazelcast-storage-autoconfigure/src/main/kotlin/io/github/ktakashi/oas/storages/hazelcast/configurations/AutoHazelcastStorageConfigurations.kt
+++ app/spring/oas-stub-hazelcast-storage-autoconfigure/src/main/kotlin/io/github/ktakashi/oas/storages/hazelcast/configurations/AutoHazelcastStorageConfigurations.kt
@@ -1,5 +1,7 @@
 package io.github.ktakashi.oas.storages.hazelcast.configurations
 
+import com.fasterxml.jackson.annotation.JsonSubTypes
+import com.fasterxml.jackson.annotation.JsonTypeInfo
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.hazelcast.client.HazelcastClient
@@ -42,16 +44,17 @@ import org.springframework.context.annotation.Configuration
 @ConditionalOnPropertyIsEmpty("spring.hazelcast.config")
 @EnableConfigurationProperties(HazelcastStorageProperties::class)
 class AutoHazelcastConfiguration(private val properties: HazelcastStorageProperties) {
+
     @Bean(destroyMethod = "shutdown")
     @ConditionalOnMissingBean
     fun hazelcastInstance(objectMapper: ObjectMapper): HazelcastInstance {
         val instance = properties.instance
-        val config = instance?.let(this::initClientConfig) ?: ClientConfig.load()
+        val config = instance?.let { initClientConfig(it, objectMapper) } ?: ClientConfig.load()
         val serializer = JsonSerializer(objectMapper, properties.typeId)
         addSerializer(config, serializer)
         return Optional.ofNullable(instance)
                 .map<HazelcastInstanceProperties>(HazelcastMainInstanceProperties::failover)
-                .map(this::initClientConfig)
+                .map { i -> initClientConfig(i, objectMapper) }
                 .map { c -> addSerializer(c, serializer) }
                 .map { fo -> ClientFailoverConfig().addClientConfig(config).addClientConfig(fo) }
                 .map(HazelcastClient::newHazelcastFailoverClient)
@@ -64,17 +67,22 @@ class AutoHazelcastConfiguration(private val properties: HazelcastStoragePropert
             typeClass = JsonNode::class.java
         })
     }
-    private fun initClientConfig(instance: HazelcastInstanceProperties) = ClientConfig().apply {
-        if (instance.username != null && instance.password != null) {
-            setCredentials(UsernamePasswordCredentials(instance.username, instance.password))
+    private fun initClientConfig(instance: HazelcastInstanceProperties, objectMapper: ObjectMapper): ClientConfig = ClientConfig().apply {
+        val credentials = instance.credentials?.let { m -> objectMapper.convertValue(m, HazelcastCredentials::class.java) }
+        when (credentials) {
+            is HazelcastUsernamePasswordCredentials -> if (credentials.username != null && credentials.password != null) {
+                setCredentials(UsernamePasswordCredentials(credentials.username, credentials.password))
+            }
+            else -> {}
         }
+
         if (instance.name != null) {
             instanceName = instance.name
         }
         if (instance.clusterName != null) {
             clusterName = instance.clusterName
         }
-        networkConfig.addresses = instance.nodeIps?.toList() ?: listOf()
+        networkConfig.addresses = instance.nodeIps?: listOf()
         networkConfig.isSmartRouting = true
     }
 }
@@ -143,17 +151,9 @@ sealed interface HazelcastInstanceProperties {
     val name: String?
 
     /**
-     * Username
-     *
-     * NOTE: if you need other authentication method,
-     * provide own [HazelcastInstance] bean
+     * Credential
      */
-    val username: String?
-
-    /**
-     * Password
-     */
-    val password: String?
+    val credentials: Map<String, Any>?
 
     /**
      * Cluster name
@@ -163,54 +163,22 @@ sealed interface HazelcastInstanceProperties {
     /**
      * Node IPs
      */
-    val nodeIps: Array<String>?
-
-    fun areEqual(other: HazelcastInstanceProperties): Boolean {
-        if (name != other.name) return false
-        if (username != other.username) return false
-        if (password != other.password) return false
-        if (clusterName != other.clusterName) return false
-        if (nodeIps != null) {
-            if (other.nodeIps == null) return false
-            if (!nodeIps.contentEquals(other.nodeIps)) return false
-        } else if (other.nodeIps != null) return false
-
-        return true
-    }
-
-    fun hash(): Int {
-        var result = name?.hashCode() ?: 0
-        result = 31 * result + (username?.hashCode() ?: 0)
-        result = 31 * result + (password?.hashCode() ?: 0)
-        result = 31 * result + (clusterName?.hashCode() ?: 0)
-        result = 31 * result + (nodeIps?.contentHashCode() ?: 0)
-        return result
-    }
+    val nodeIps: List<String>?
 }
 data class HazelcastFailoverInstanceProperties(override val name: String?,
-                                               override val username: String?,
-                                               override val password: String?,
+                                               @NestedConfigurationProperty override val credentials: Map<String, Any>?,
                                                override val clusterName: String?,
-                                               override val nodeIps: Array<String>?): HazelcastInstanceProperties {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-        return areEqual(other as HazelcastInstanceProperties)
-    }
-
-    override fun hashCode(): Int = hash()
-
+                                               override val nodeIps: List<String>?): HazelcastInstanceProperties {
     override fun toString(): String {
-        return "HazelcastFailoverInstanceProperties(name=$name, username=$username, clusterName=$clusterName, nodeIps=${nodeIps?.contentToString()})"
+        return "HazelcastFailoverInstanceProperties(name=$name, clusterName=$clusterName, nodeIps=$nodeIps)"
     }
 }
 
 data class HazelcastMainInstanceProperties(
     override val name: String?,
-    override val username: String?,
-    override val password: String?,
+    @NestedConfigurationProperty override val credentials: Map<String, Any>?,
     override val clusterName: String?,
-    override val nodeIps: Array<String>?,
+    override val nodeIps: List<String>?,
     /**
      * Failover config.
      *
@@ -218,21 +186,31 @@ data class HazelcastMainInstanceProperties(
      */
     val failover: HazelcastFailoverInstanceProperties?
 ): HazelcastInstanceProperties {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as HazelcastMainInstanceProperties
-        return areEqual(other) && failover == other.failover
-    }
-
-    override fun hashCode(): Int {
-        var result = hash()
-        result = 31 * result + (failover?.hashCode() ?: 0)
-        return result
-    }
-
     override fun toString(): String {
-        return "HazelcastMainInstanceProperties(name=$name, username=$username, clusterName=$clusterName, nodeIps=${nodeIps?.contentToString()}, failover=$failover)"
+        return "HazelcastMainInstanceProperties(name=$name, clusterName=$clusterName, nodeIps=$nodeIps, failover=$failover)"
+    }
+}
+
+@JsonTypeInfo(
+    use = JsonTypeInfo.Id.NAME,
+    property = "type",
+    defaultImpl = HazelcastUsernamePasswordCredentials::class
+)
+@JsonSubTypes(
+    JsonSubTypes.Type(value = HazelcastUsernamePasswordCredentials::class, name = "password")
+)
+sealed interface HazelcastCredentials
+data class HazelcastUsernamePasswordCredentials(
+    /**
+     * Username
+     */
+    val username: String?,
+    /**
+     * Password
+     */
+    val password: String?
+): HazelcastCredentials {
+    override fun toString(): String {
+        return "HazelcastCredentials[username=$username]"
     }
 }
