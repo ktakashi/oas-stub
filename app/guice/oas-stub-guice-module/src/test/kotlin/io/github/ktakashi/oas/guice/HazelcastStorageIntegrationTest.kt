e66b9d0a7c20e5e22f1c54382a20b009e985634f
Takashi Kato <ktakashi@ymail.com>
2023-10-02 16:17:45 +0200

    Are these the culprits?

--- app/guice/oas-stub-guice-module/src/test/kotlin/io/github/ktakashi/oas/guice/HazelcastStorageIntegrationTest.kt
+++ app/guice/oas-stub-guice-module/src/test/kotlin/io/github/ktakashi/oas/guice/HazelcastStorageIntegrationTest.kt
@@ -2,11 +2,13 @@ package io.github.ktakashi.oas.guice
 
 import io.cucumber.junit.platform.engine.Constants
 import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Disabled
 import org.junit.platform.suite.api.ConfigurationParameter
 import org.junit.platform.suite.api.IncludeEngines
 import org.junit.platform.suite.api.SelectClasspathResource
 import org.junit.platform.suite.api.Suite
 
+@Disabled
 @Suite
 @IncludeEngines(value = ["cucumber"])
 @SelectClasspathResource("features")
--- app/guice/oas-stub-guice-module/src/test/kotlin/io/github/ktakashi/oas/guice/InMemoryStorageIntegrationTest.kt
+++ app/guice/oas-stub-guice-module/src/test/kotlin/io/github/ktakashi/oas/guice/InMemoryStorageIntegrationTest.kt
@@ -1,11 +1,13 @@
 package io.github.ktakashi.oas.guice
 
 import io.cucumber.junit.platform.engine.Constants
+import org.junit.jupiter.api.Disabled
 import org.junit.platform.suite.api.ConfigurationParameter
 import org.junit.platform.suite.api.IncludeEngines
 import org.junit.platform.suite.api.SelectClasspathResource
 import org.junit.platform.suite.api.Suite
 
+@Disabled
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
