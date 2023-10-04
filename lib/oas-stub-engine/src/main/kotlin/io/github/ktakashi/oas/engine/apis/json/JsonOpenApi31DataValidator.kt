c5d8ece9e93ad6af14641adb8953abad950affb9
Takashi Kato <ktakashi@ymail.com>
2023-10-04 16:41:30 +0200

    Initial OAS 3.1.x validator

--- lib/oas-stub-engine/src/main/kotlin/io/github/ktakashi/oas/engine/apis/json/JsonOpenApi31DataValidator.kt
+++ lib/oas-stub-engine/src/main/kotlin/io/github/ktakashi/oas/engine/apis/json/JsonOpenApi31DataValidator.kt
@@ -2,31 +2,98 @@ package io.github.ktakashi.oas.engine.apis.json
 
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.node.ArrayNode
+import com.fasterxml.jackson.databind.node.BooleanNode
+import com.fasterxml.jackson.databind.node.NullNode
+import com.fasterxml.jackson.databind.node.NumericNode
+import com.fasterxml.jackson.databind.node.ObjectNode
+import com.fasterxml.jackson.databind.node.TextNode
 import io.github.ktakashi.oas.engine.apis.AbstractApiDataValidator
 import io.github.ktakashi.oas.engine.apis.ApiValidationResult
 import io.github.ktakashi.oas.engine.apis.failedResult
 import io.github.ktakashi.oas.engine.apis.success
+import io.github.ktakashi.oas.engine.validators.StringValidationContext
 import io.github.ktakashi.oas.engine.validators.Validator
 import io.swagger.v3.oas.models.SpecVersion
+import io.swagger.v3.oas.models.media.JsonSchema
 import io.swagger.v3.oas.models.media.Schema
 import jakarta.inject.Inject
 import jakarta.inject.Named
 import jakarta.inject.Singleton
 import java.io.IOException
+import java.util.Optional
+import java.util.regex.Pattern
 
 @Named @Singleton
 class JsonOpenApi31DataValidator
 @Inject constructor(private val objectMapper: ObjectMapper,
                     private val validators: Set<Validator<Any>>) : AbstractApiDataValidator<JsonNode>(SpecVersion.V31), JsonMediaSupport {
-    // TODO implement it,
-    // NOTE since OAS 3.1.x, it only returns JsonSchema, so we need to implement JSON Schema :(
-    //      we might be able to use one of the implementation listed here
-    //      - https://json-schema.org/implementations.html#validator-java
+
     override fun validate(input: ByteArray, schema: Schema<*>): ApiValidationResult = try {
-        checkSchema(objectMapper.readTree(input), "$", schema)
+        checkSchema(objectMapper.readTree(input), "$", schema as JsonSchema)
     } catch (e: IOException) {
         failedResult(e.message as String)
     }
-    override fun checkSchema(value: JsonNode, rootProperty: String, schema: Schema<*>): ApiValidationResult = success  // dummy
+    override fun checkSchema(value: JsonNode, rootProperty: String, schema: Schema<*>): ApiValidationResult = when (schema) {
+        is JsonSchema -> when {
+            schema.anyOf != null -> schema.anyOf.firstOrNull { s -> checkSchema(value, rootProperty, s).isValid }
+                ?.let { success }
+                ?: failedResult("$value must satisfy at least one of ${toPrettyString(schema.anyOf)}", rootProperty)
+            else -> checkJsonSchema(value, rootProperty, schema)
+        }
+        else -> failedResult("[BUG] schema is not JsonSchema", rootProperty)
+    }
+
+    private fun checkJsonSchema(value: JsonNode, property: String, schema: JsonSchema): ApiValidationResult = when (value) {
+        is ObjectNode -> checkObject(value, property, schema)
+        is ArrayNode -> checkArray(value, property, schema)
+        is TextNode -> checkString(value, property, schema)
+        is BooleanNode -> checkBoolean(value, property, schema)
+        is NumericNode -> checkNumber(value, property, schema)
+        is NullNode -> checkNull(value, property, schema)
+        else -> failedResult("Unknown value: $value", property)
+    }
+
+    private fun checkNull(value: NullNode, property: String, schema: JsonSchema): ApiValidationResult = if (hasType("null", schema)) {
+        success
+    } else failedResult("Not a null '$value'", property)
+
+    private fun checkNumber(value: NumericNode, property: String, schema: JsonSchema): ApiValidationResult = if (hasType("integer", schema) || hasType("number", schema)) {
+        checkNumberRange(value, property, schema)
+    } else failedResult("Not a number/integer '$value'", property)
+
+    private fun checkBoolean(value: BooleanNode, property: String, schema: JsonSchema): ApiValidationResult = if (hasType("boolean", schema)) {
+        success
+    } else failedResult("Not a boolean '$value'", property)
+
+    private fun checkString(value: TextNode, property: String, schema: JsonSchema): ApiValidationResult = if (hasType("string", schema)) {
+        checkText(value, property, schema, validators) { _, textValue ->
+            schema.format?.let {
+                StringValidationContext(textValue, it)
+            } ?: StringValidationContext(textValue, "format", Optional.ofNullable(schema.pattern).map(Pattern::compile).orElse(null))
+        }
+    } else {
+        failedResult("Not a string '$value'", property)
+    }
+
+    private fun checkArray(value: ArrayNode, property: String, schema: JsonSchema): ApiValidationResult = if (hasType("array", schema)) {
+        checkElements(value, property, schema, ::checkSchema)
+    } else failedResult("Not an array '$value'", property)
+
+    private fun checkObject(value: ObjectNode, property: String, schema: JsonSchema): ApiValidationResult = if (hasType("object", schema)) {
+        val required = checkRequired(value, property, schema)
+        val result = checkProperties(value, property, schema, ::checkSchema)
+        required.merge(result)
+    } else {
+        failedResult("Not an object '$value'", property)
+    }
+
+    private fun hasType(type: String, schema: JsonSchema) = checkType(type, schema) || checkTypes(type, schema)
+
+    private fun checkTypes(type: String, schema: JsonSchema): Boolean = schema.types?.contains(type) ?: false
+
+    private fun checkType(type: String, schema: JsonSchema): Boolean = schema.type?.let {
+        it == type
+    } ?: false
 
 }
--- lib/oas-stub-engine/src/test/kotlin/io/github/ktakashi/oas/engine/apis/json/JsonApiDataValidatorTest.kt
+++ lib/oas-stub-engine/src/test/kotlin/io/github/ktakashi/oas/engine/apis/json/JsonApiDataValidatorTest.kt
@@ -1,6 +1,8 @@
 package io.github.ktakashi.oas.engine.apis.json
 
+import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
+import io.github.ktakashi.oas.engine.apis.ApiDataValidator
 import io.github.ktakashi.oas.engine.readBinaryContent
 import io.github.ktakashi.oas.engine.readStringContent
 import io.swagger.v3.core.util.Yaml
@@ -19,7 +21,8 @@ import org.junit.jupiter.params.provider.CsvSource
 import org.junit.platform.commons.util.Preconditions
 
 class JsonApiDataValidatorTest {
-    private val jsonApiDataValidator = JsonOpenApi30DataValidator(ObjectMapper(), setOf())
+    private val jsonApi30DataValidator = JsonOpenApi30DataValidator(ObjectMapper(), setOf())
+    private val jsonApi31DataValidator = JsonOpenApi31DataValidator(ObjectMapper(), setOf())
     private val openApiV3Parser = OpenAPIV3Parser()
     private val swaggerConverter = SwaggerConverter()
     private val parserOptions = ParseOptions().also {
@@ -30,10 +33,26 @@ class JsonApiDataValidatorTest {
     @CsvSource(value = [
         "v3,/schema/validation_3.0.3.yaml,/json/valid_input0.json,true",
         "v3,/schema/validation_3.0.3.yaml,/json/valid_input1.json,true",
-        "v3,/schema/validation_3.0.3.yaml,/json/invalid_input0.json,false,$.id,$.values[0]"
+        "v3,/schema/validation_3.0.3.yaml,/json/invalid_input0.json,false,$.id,$.values[0]",
     ])
-    fun validate(type: String, schemaFile: String, inputFile: String, isValid: Boolean,
+    fun validate30(type: String, schemaFile: String, inputFile: String, isValid: Boolean,
                  @AggregateWith(VarargsAggregator::class) vararg properties: String) {
+        validate(jsonApi30DataValidator, type, schemaFile, inputFile, isValid, *properties)
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = [
+        "v3,/schema/validation_3.1.0.yaml,/json/valid_input0.json,true",
+        "v3,/schema/validation_3.1.0.yaml,/json/valid_input1.json,true",
+        "v3,/schema/validation_3.1.0.yaml,/json/invalid_input0.json,false,$.id,$.values[0]",
+        "v3,/schema/validation_3.1.0.yaml,/json/valid_input2.json,true",
+    ])
+    fun validate31(type: String, schemaFile: String, inputFile: String, isValid: Boolean,
+                 @AggregateWith(VarargsAggregator::class) vararg properties: String) {
+        validate(jsonApi31DataValidator, type, schemaFile, inputFile, isValid, *properties)
+    }
+
+    private fun validate(validator: ApiDataValidator<JsonNode>, type: String, schemaFile: String, inputFile: String, isValid: Boolean, vararg properties: String) {
         val openAPI = when (type) {
             "v3" -> openApiV3Parser.readContents(readStringContent(schemaFile), null, parserOptions).openAPI
             "v2" -> swaggerConverter.readContents(readStringContent(schemaFile), null, parserOptions).let {
@@ -43,7 +62,7 @@ class JsonApiDataValidatorTest {
             else -> throw IllegalArgumentException("$type is not supported... obviously")
         }
         val schema = openAPI.paths["/object"]?.post?.requestBody?.content?.get("application/json")?.schema ?: throw IllegalArgumentException("Watch out")
-        val result = jsonApiDataValidator.validate(readBinaryContent(inputFile), schema)
+        val result = validator.validate(readBinaryContent(inputFile), schema)
         assertEquals(isValid, result.isValid)
         assertEquals(properties.toList(), result.validationDetails.map { v -> v.property.get() })
     }
