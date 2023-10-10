6834d768d46cf00fffbd3dae472d5a3fbbe3db0d
Takashi Kato <ktakashi@ymail.com>
2023-10-10 17:01:14 +0200

    Remove unused import

--- lib/oas-stub-engine/src/main/kotlin/io/github/ktakashi/oas/engine/apis/json/JsonOpenApi31DataPopulator.kt
+++ lib/oas-stub-engine/src/main/kotlin/io/github/ktakashi/oas/engine/apis/json/JsonOpenApi31DataPopulator.kt
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.node.NullNode
 import com.fasterxml.jackson.databind.node.ObjectNode
-import com.fasterxml.jackson.databind.node.TextNode
 import io.github.ktakashi.oas.engine.apis.AbstractApiDataPopulator
 import io.github.ktakashi.oas.engine.apis.ApiAnyDataPopulator
 import io.swagger.v3.oas.models.SpecVersion
