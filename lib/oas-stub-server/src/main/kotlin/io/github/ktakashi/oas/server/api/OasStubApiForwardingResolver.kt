b878e045c6d50f4593eb6465fd5959008deecb60
Takashi Kato <ktakashi@ymail.com>
2024-07-08 14:53:41 +0200

    Adding request forwarding document

--- lib/oas-stub-server/src/main/kotlin/io/github/ktakashi/oas/server/api/OasStubApiForwardingResolver.kt
+++ lib/oas-stub-server/src/main/kotlin/io/github/ktakashi/oas/server/api/OasStubApiForwardingResolver.kt
@@ -5,10 +5,31 @@ import io.github.ktakashi.oas.server.options.OasStubStubOptions
 import java.net.URLEncoder
 import java.nio.charset.StandardCharsets
 
+/**
+ * API name resolver.
+ *
+ * This resolver is used when the `forwardingPath` option
+ * is provided.
+ */
 fun interface OasStubApiForwardingResolver {
+    /**
+     * Resolve the stub name.
+     *
+     * [requestUri]: the original request URI
+     * [request]: an [HttpRequest] instance.
+     * [options]: Stub options
+     *
+     * NOTE: Calling `request.requestURI` causes infinite loop. If you need
+     * the original URI, then use the [requestUri].
+     */
     fun resolveRequestUri(requestUri: String, request: HttpRequest, options: OasStubStubOptions): String?
 }
 
+/**
+ * Header value forwarding resolver.
+ *
+ * The value of the [headerName] will be the stub name.
+ */
 class OasStubApiByHeaderForwardingResolver(private val headerName: String) : OasStubApiForwardingResolver {
     override fun resolveRequestUri(requestUri: String, request: HttpRequest, options: OasStubStubOptions): String? = request.getHeader(headerName)?.let { name ->
         // simply append
